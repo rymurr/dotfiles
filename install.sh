7c9b2e7e854ee61c6e14b7d0cd79cce1ac008f0b
Ryan Murray <rymurr@gmail.com>
2019-04-18 19:39:14 +0100

    Thu 18 Apr 19:39:14 BST 2019

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | /bin/bash
 #test this
-# curl -o- https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh | bash
-# browserpass
+curl -o- https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh > anaconda.sh
+./anaconda.sh
 # zoom.deb
 # rescutime.deb
-# chezmoi.deb
-# pip install thefuck powerline
-# neovim
-# spacevim
 # all the yubikey stuff
-# wavebox
-# fonts
+# browserpass
+pip install thefuck powerline-status wakatime i3-py
+curl -sLf https://spacevim.org/install.sh | bash
